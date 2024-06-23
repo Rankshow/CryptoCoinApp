2e387aa1a6ab991144caa3d6ee946b4ddfb571ec
Rankshow <uwaomaobinna20@gmail.com>
2024-06-23 02:53:48 +0100

    Added an interface

---
+++ CryptoCoinApp/Interface/IHttpConnection.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoCoinApp.Interface
+{
+    public interface IHttpConnection
+    {
+        Task<T> WebRequest<T>(
+            string url,
+            string requestType,
+            object? requestBody = null,
+            Dictionary<string, string>? headers = null,
+            string? authUser = null,
+            string? authPassword = null) where T : new();
+    }
+}
